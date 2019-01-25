cc5395bc3caed2522b0d48e88e6081c7bf8715a8
Rafael Galleani <rafegal@gmail.com>
2019-01-25 10:18:10 -0200

    Commit da primeira aplicação Django. Retornando um Hello World

---
+++ core/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def hello(request, nome, idade):
+    return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
